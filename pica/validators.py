aa377b9d5bb73aa1d1aa46c7596b16b331a21249
setiawankarnata <setiawankarnata@gmail.com>
2021-11-29 08:50:24 +0700

    Penambahan keterangan pada validators.py

--- pica/validators.py
+++ pica/validators.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import ValidationError
 
 
+# Digunakan untuk memvalidasi fields yang ada di models.py
+
 def validate_empty(value):
     if value == "":
         raise ValidationError("{} tidak boleh dikosongkan.".format(value))
